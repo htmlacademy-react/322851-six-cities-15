0094951dc1e8eccd292f9fe0e0554a21a0b24034
SJ23y <mcfolland@gmail.com>
2024-02-20 01:38:57 +0300

    Созданы компоненты все страниц доступных в приложении. Создан компонент карточки места размещения

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import Main from '../../pages/main/main-page';
+
+type AppProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppProps): JSX.Element {
+  return (
+    <Main offersCount={offersCount} />
+  );
+}
+
+
+export default App;
